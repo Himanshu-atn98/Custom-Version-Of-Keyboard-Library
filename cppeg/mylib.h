a0db711d347e20a6e5be3e48c9e14e26cd14683a
Himanshu Atneriya <himanshuatneriya98@gmail.com>
2024-12-19 03:23:11 +0530

    Delete cppeg directory

--- cppeg/mylib.h
+++
@@ -1,6 +0,0 @@
-int getStringLength(char *);
-void toUpperCase(char *);
-void toLowerCase(char *);
-void copyString(char *,char *);
-void reverseString(char *);
-void concatenateString(char *,char *);
